6e99d47d826836c225ec64f001e5f1c8ac924a65
Eduardoryt <20221076@uthh.edu.mx>
2024-08-07 10:29:20 -0600

    Delete .history directory

--- .history/src/api/folio_20240708173638.js
+++
@@ -1,5 +0,0 @@
-import axios from "./axios";
-
-export const getfolioInterno = async () => axios.get(`/folio/ultimo-folio-counter`);
-
-export const getfolioInternoInforme = async () => axios.get(`/folio/ultimo-folio-counter-informe`);
--- .history/src/api/historialInput_20240708173742.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const gethistorialOrdenTrabajo = async () => axios.get(`/historialInput.`);
-
-export const getfolioInternoInforme = async () => axios.get(`/folio/ultimo-folio-counter-informe`);
