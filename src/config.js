51c53c1851c4cc461885dc7b2bcb5782404edcb0
Eduardoryt <20221076@uthh.edu.mx>
2024-08-10 00:04:13 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL|| "proyecto-estadia-siec-kgvbnpzbg-eduardoryts-projects.vercel.app";
+export const API_URL = import.meta.env.VITE_API_URL|| "http://localhost:4000/api";
