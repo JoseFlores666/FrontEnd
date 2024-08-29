fcddcd4011bb1617f5d7fa9e139cfd9289381d8b
JoseFlores666 <136219170+JoseFlores666@users.noreply.github.com>
2024-08-29 10:25:55 -0600

    Update LlenarSolicitud.jsx

--- src/components/ui/LlenarSolicitud.jsx
+++ src/components/ui/LlenarSolicitud.jsx
@@ -122,21 +122,21 @@ closeModal,
     };
 
     const generatePDFDocument = async () => {
+          Swal.fire({
+                    title: "Descarga Exitosa",
+                    text: "Archivo PDF generado con éxito",
+                    icon: "success",
+                    confirmButtonText: "OK",
+                });
+                navigate('/soli');
         setError(null);
         try {
             if (api_Key.length > 0) {
-                   navigate('/soli');
                 const apiKey = api_Key[0].api_key;
                 const docxBlob = await fetchAndGenerateDoc();
                 const pdfBlob = await apiPDF(docxBlob,apiKey);
                 const pdfUrl = URL.createObjectURL(pdfBlob);
                 window.open(pdfUrl, '_blank');
-                Swal.fire({
-                    title: "Descarga Exitosa",
-                    text: "Archivo PDF generado con éxito",
-                    icon: "success",
-                    confirmButtonText: "OK",
-                })
             }
         } catch (error) {
             console.error(error);
